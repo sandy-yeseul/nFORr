aa71f8a16fac943c104b7ca43f1b62b7ceffb5b7
zimzim <zimzim@youcanstar.com>
2020-10-12 15:43:02 +0900

    initial workflow set

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Book = require('./models/book');
 const app = express();
-const port = 3000
+const port = 3028;
 
+mongoose.connect('mongodb+srv://Dalga:4nightMare4@cluster0.a3rfz.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
+    .then(()=> console.log('Mongodb Connected...'))
+    .catch(err => console.log(err));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
+// routes
+app.get('/', (req, res) =>{
+    res.send('hello,,,,')
+});
+app.post('/book', (req, res) =>{
+    console.log(req.body);
+    const book = new Book(req.body);
+    book.save((err, book) =>{
+        if(err) return res.json({success: false, err});
+        return res.status(201).json({success: true, book})
+    })
+})
+app.get('/book', (req, res)=>{
+    Book.get(function(err, books){
+        if(err) return res.json({success: false, err});
+        return res.json({success: true, message: "Successfully get book list", data: books});
+    })
+})
 app.listen(port, () => console.log(`Portfolio application on port ${port}`));
-
-/**
- * 1. import express
- * 2. listen to the port
- * 3. connect to mongodb
- * 4. a simple route for index
- * 5. 
- */
---
+++ models/book.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    publisher: String,
+    seller: String,
+    price: Number,
+    publishDate: Date
+});
+
+const Book = module.exports =mongoose.model('Book', bookSchema);
+module.exports.get = function (callback, limit) {
+    Book.find(callback).limit(limit);
+}
