66bd8fd714dc71f3c1b51ba17acd9f2561fffcca
zimzim <zimzim@youcanstar.com>
2020-10-26 13:41:33 +0900

    landing page css

---
+++ client/src/components/Pages/homePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+function homePage() {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
+
+export default withRouter(homePage)
