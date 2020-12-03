4bde2714694246f9fd6a6a7951838bbeef224c35
zimzim <zimzim@youcanstar.com>
2020-12-03 17:18:05 +0900

    add alert

--- client/src/components/Common/Sync.js
+++ client/src/components/Common/Sync.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
+import Collapse from "@material-ui/core/Collapse";
+import CloseIcon from "@material-ui/icons/Close";
+import Alert from "@material-ui/lab/Alert";
 import SyncIcon from "@material-ui/icons/Sync";
 import { callDb } from "../../utilities";
 
 export default function Sync({ books }) {
   const [animate, setAnimate] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState('');
   const SyncHandler = async () => {
     const params = {
       method: "POST",
@@ -13,9 +18,26 @@ export default function Sync({ books }) {
       body: books,
     };
     const result = await callDb(params);
+    setContent(result.data)
+    setOpen(true)
   };
   return (
     <>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >{content}
+        </Alert>
+      </Collapse>
       <Tooltip title="Figure out if book has been published">
         <IconButton
           variant="outlined"
